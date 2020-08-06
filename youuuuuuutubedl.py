c19471c584196c3b7da4a6a0f4ffbf46e30e6670
ainiml <naisanza@gmail.com>
2020-08-06 16:17:31 -0400

    updated logging

--- youuuuuuutubedl.py
+++ youuuuuuutubedl.py
@@ -10,24 +10,16 @@ import stat
 import time
 import psutil
 import shutil
-import logging
 import requests
-import threading
 import subprocess
-import multiprocessing
 
-from sys import stdout
 from queue import Queue
 from shutil import move
 from subprocess import PIPE
 from bs4 import BeautifulSoup
 from concurrent.futures import (ThreadPoolExecutor, wait, as_completed)
 
-
-FORMAT = '%(asctime)s  [%(levelname)s ] %(message)s'
-FORMAT = '[%(levelname)s ] %(message)s'
-FORMAT = '%(message)s'
-logging.basicConfig(filename=None, format=FORMAT, level=logging.INFO)
+from automon.log import Logging
 
 
 class Youtube:
@@ -35,6 +27,8 @@ class Youtube:
     def __init__(self, thread_pool=None, urls_file=None):
         """A multithreaded wrapper for youtube-dl
         """
+
+        self._log = Logging(Youtube.__name__, self._log.debug)
 
         # Directories
         self.path = os.path.split(os.path.realpath(__file__))[0]
@@ -48,10 +42,10 @@ class Youtube:
                 os.makedirs(directory)
             if directory == self.dir_d:
                 for directory in os.listdir(self.dir_d):
-                    pass
                     # Don't clean up previous downloads
                     # Clean out previous downloads
                     # os.remove(self.dir_d + '/' + directory)
+                    pass
 
         self.urls_file = urls_file
         self.urls = self._url_builder() or list()
@@ -61,7 +55,7 @@ class Youtube:
         self.yt = os.path.join('bin', 'youtube-dl')
         self.yt_name = ' --get-filename -o ' + self.dir_d + '/%(title)s.%(ext)s '
         self.yt_args = ' -o ' + self.dir_d + '/%(title)s.%(ext)s '
-        self.yt_audio_args = yt_args + ' --extract-audio --audio-format mp3 -k '
+        self.yt_audio_args = self.yt_args + ' --extract-audio --audio-format mp3 -k '
 
         # Thread pool
         if thread_pool:
@@ -78,40 +72,40 @@ class Youtube:
         # Run Downloader
         self.futures = list()
         for url in self.urls:
-            logging.info('[Youtube] added {}'.format(url))
+            self._log.info('[Youtube] added {}'.format(url))
             self.queue.put(url)
 
         urls = len(self.urls)
 
         if urls > 1:
-            logging.info('[Youtube] {} urls added to queue'.format(urls))
+            self._log.info('[Youtube] {} urls added to queue'.format(urls))
         else:
-            logging.info('[Youtube] {} url added to queue'.format(urls))
+            self._log.info('[Youtube] {} url added to queue'.format(urls))
 
         sleep = 0
         while True:
             if self.queue.empty():
                 break
 
-            if self._cpu_usage():
+            if self._cpu_usage(80):
                 url = self.queue.get()
                 self.futures.append(self.pool.submit(self._downloader, url))
                 urls = urls - 1
-                logging.info('[Youtube] {} left'.format(urls))
+                self._log.info('[Youtube] {} left'.format(urls))
                 sleep = int(sleep / 2)
             else:
                 sleep += int(sleep + 1 * 2)
-                # logging.debug('[_downloader] Sleeping for: {} seconds'.format(sleep))
+                # self._log.debug('[_downloader] Sleeping for: {} seconds'.format(sleep))
                 time.sleep(sleep)
 
     def _cpu_usage(self, max_cpu_percentage=80):
         """Limit max cpu usage
         """
         if psutil.cpu_percent() < max_cpu_percentage:
-            logging.debug('[_cpu_usage] cpu usage: {}'.format(psutil.cpu_percent()))
+            self._log.debug('[_cpu_usage] cpu usage: {}'.format(psutil.cpu_percent()))
             return True
         else:
-            logging.debug('[_cpu_usage] cpu usage: {}'.format(psutil.cpu_percent()))
+            self._log.debug('[_cpu_usage] cpu usage: {}'.format(psutil.cpu_percent()))
             return False
 
     def _url_builder(self):
@@ -286,12 +280,12 @@ class Youtube:
         downloads = list()
 
         # Download file
-        logging.info('[_downloader] Downloading url {}'.format(url))
+        self._log.info('[_downloader] Downloading url {}'.format(url))
         logs.store(run(dl))
 
         # Download audio
         # Requires ffmpeg or avconv and ffprobe or avprobe
-        logging.info('[] Downloading audio {}'.format(url))
+        self._log.info('[] Downloading audio {}'.format(url))
         logs.store(run(dl_audio))
 
         while True:
@@ -300,7 +294,7 @@ class Youtube:
 
             if line is not False:
 
-                logging.debug('[log ] {}'.format(line))
+                self._log.debug('[log ] {}'.format(line))
 
                 # find matching downloads
                 for r in regexes:
@@ -316,17 +310,17 @@ class Youtube:
                         filepath = os.path.join(self.dir_d, filename)
 
                         # don't show line unless it matches
-                        logging.debug('[_downloader] {}'.format(line))
-                        logging.debug('[regex] {}'.format(regex))
-                        logging.debug('[regex] {}'.format(filename))
+                        self._log.debug('[_downloader] {}'.format(line))
+                        self._log.debug('[regex] {}'.format(regex))
+                        self._log.debug('[regex] {}'.format(filename))
 
                         if os.path.exists(filepath):
                             if r_type == 'finished':
-                                logging.info('[_downloader] finished: {}'.format(filename))
+                                self._log.info('[_downloader] finished: {}'.format(filename))
                                 if filename not in finished:
                                     finished.append(filename)
                             else:
-                                logging.info('[_downloader] downloading: {}'.format(filename))
+                                self._log.info('[_downloader] downloading: {}'.format(filename))
                                 if filename not in downloads:
                                     downloads.append(filename)
                             break
@@ -336,13 +330,13 @@ class Youtube:
         # move all finished files
         self._finished(finished)
 
-        logging.info('[_downloader] took {} seconds to complete {}'.format(int(time.time() - start), url))
+        self._log.info('[_downloader] took {} seconds to complete {}'.format(int(time.time() - start), url))
 
     def _move_file(self, source, target):
         """Move file including metadata
         """
         try:
-            logging.info('[_move_file] Moving: {} ({})'.format(os.path.split(target)[-1], os.stat(source).st_size))
+            self._log.info('[_move_file] Moving: {} ({} B)'.format(os.path.split(target)[-1], os.stat(source).st_size))
 
             # copy content, stat-info (mode too), timestamps...
             shutil.copy2(source, target)
@@ -351,10 +345,10 @@ class Youtube:
             os.chown(target, st[stat.ST_UID], st[stat.ST_GID])
             os.remove(source)
 
-            logging.info('[_move_file] Moved: {}'.format(os.path.split(target)[-1]))
+            self._log.info('[_move_file] Moved: {}'.format(os.path.split(target)[-1]))
             return True
         except:
-            logging.error('[_move_file] Failed: {}'.format(os.path.split(source)[-1]))
+            self._log.error('[_move_file] Failed: {}'.format(os.path.split(source)[-1]))
             return False
 
     def _finished(self, finished):
@@ -364,7 +358,7 @@ class Youtube:
             source = os.path.join(self.dir_d, file)
             target = os.path.join(self.dir_f, file)
 
-            logging.info('[_finished] {}'.format(file))
+            self._log.info('[_finished] {}'.format(file))
             self._move_file(source, target)
 
     def _cookie_builder(self, cookies):
@@ -403,173 +397,174 @@ class Youtube:
         return cookies
 
 
-q = Queue()
+#
+# q = Queue()
+#
+# # relative paths
+# script = os.path.realpath(__file__)
+# path = os.path.split(script)[0]
+# d = path + r'/files/downloading'
+# f = path + r'/files/finished'
+# p = path + r'/files/pending'
+# c = path + r'/files/cookies'
+# # bin/youtube-dl --get-filename -o files/downloading/'%(title)s.%(ext)s' --restrict-filenames
+# yt = r'bin/youtube-dl'
+# yt_name = ' --get-filename -o ' + d + '/%(title)s.%(ext)s '
+# yt_args = ' -o ' + d + '/%(title)s.%(ext)s '
+# yt_audio_args = yt_args + ' --extract-audio --audio-format mp3 -k '
+#
+#
+# # No spaces in filename
+# # yt_args = ' -o ' + d + '/%(title)s.%(ext)s ' + '--restrict-filenames '
+# # yt_args = ' --get-filename -o \'' + d + '/%(title)s.%(ext)s\' ' + '--restrict-filenames '
+#
+#
+# # This will be depreciated in the version 1.3
+# def go(thread_pool):
+#     """Go, go, go!
+#     """
+#
+#     dirs = [d, f, p, c]
+#
+#     for _ in dirs:
+#         if not os.path.exists(_):
+#             os.makedirs(_)
+#         if _ == d:
+#             for _ in os.listdir(d):
+#                 pass
+#                 # Clean out previous downloads
+#                 # os.remove(d + '/' + _)
+#
+#     return run_queue()
+#
+#
+# # This will be depreciated in the version 1.3
+# def list_builder():
+#     """Create a clean list of urls
+#
+#     You can put any text file or html file into files/pending
+#
+#     The list bulider will take any file inside of files/pending
+#     and create a list of URLs from it
+#     """
+#
+#     cookies = ops.cookie_jar(c)
+#     urls = ops.read_files(p)
+#
+#     return urls
+#
+#
+# # This will be depreciated in the version 1.3
+# def downloader(url):
+#     """Download the url
+#     """
+#
+#     # run youtube-dl
+#     # os.chmod(yt, 0o555)
+#     dl = yt + yt_args + url
+#     dl_audio = yt + yt_audio_args + url
+#     # subprocess.Popen(url.split(), stdout=PIPE, stderr=PIPE)
+#
+#     t_name = threading.current_thread().getName()
+#
+#     try:
+#         # Download file
+#         # TODO: bug - Unable to parse through subprocess stdout output
+#         print('[*] [' + t_name + ']', 'Running command:', dl)
+#         print('[*] [' + t_name + ']', 'Downloading:', url)
+#         _stdout, _stderr = subprocess.Popen(dl.split(), stdout=PIPE, stderr=PIPE).communicate()
+#         print('[*] [' + t_name + ']', _stdout.decode())
+#         print('[*] [' + t_name + ']', _stderr.decode())
+#         # [download] Destination:
+#         # regex = '^(?<=\[download\] Destination: ).*$'
+#
+#         # Download audio
+#         # TODO: Download audio format
+#         # Requires ffmpeg or avconv and ffprobe or avprobe
+#         print('[*] [' + t_name + ']', 'Running command:', dl_audio)
+#         print('[*] [' + t_name + ']', 'Converting to mp3:', url)
+#         _stdout, _stderr = subprocess.Popen(dl_audio.split(), stdout=PIPE, stderr=PIPE).communicate()
+#         print('[*] [' + t_name + ']', _stdout.decode())
+#         print('[*] [' + t_name + ']', _stderr.decode())
+#
+#     except:
+#         raise
+#
+#
+# # This will be depreciated in the version 1.3
+# def run_queue(thread_pool):
+#     """Start queue processing
+#     """
+#
+#     for _ in list_builder():
+#         print('[*]', 'Added to queue:', _)
+#         q.put(_)
+#
+#     print('[*]', q.qsize(), 'items in queue')
+#
+#     def threader(threads=q.qsize()):
+#         """Create threads
+#         """
+#
+#         ts = []
+#         for _ in range(threads):
+#             t = threading.Thread(target=worker)
+#             # t.setDaemon(True)
+#             t.start()
+#             ts.append(t)
+#
+#         print('[*] Threads started', len(ts))
+#
+#         # Stop threads
+#         for _ in range(threads):
+#             q.put(None)
+#
+#         # Wait on threads to finish
+#         for _ in ts:
+#             _.join()
+#
+#         # Move completed downloads
+#         for _, __, ___ in os.walk(d):
+#             for _ in ___:
+#                 src = d + '/' + _
+#                 dst = f + '/' + _
+#                 print('[*] Finished', dst)
+#                 move(src, dst)
+#
+#         # print('[*] >>', '[' + t_name + ']', dl)
+#         # if relpath:
+#         #     move(relpath, f + '/' + dl_name_name)
+#         # if dl_path:
+#         #     move(dl_path, f + '/' + dl_path_name)
+#
+#     def worker():
+#         """Create workers
+#         """
+#
+#         while True:
+#             url = q.get()
+#
+#             if url is None:
+#                 break
+#
+#             print('[*] Running queue:', q.qsize(), 'item(s) left')
+#             thread_pool.submit(downloader, url)
+#
+#     return threader()
+#
+#
+# # This will be depreciated in the version 1.3
+# def main():
+#     """Main
+#
+#     """
+#     while True:
+#         Youtube()
+#         print('sleep for 3600')
+#         time.sleep(3600)
 
-# relative paths
-script = os.path.realpath(__file__)
-path = os.path.split(script)[0]
-d = path + r'/files/downloading'
-f = path + r'/files/finished'
-p = path + r'/files/pending'
-c = path + r'/files/cookies'
-# bin/youtube-dl --get-filename -o files/downloading/'%(title)s.%(ext)s' --restrict-filenames
-yt = r'bin/youtube-dl'
-yt_name = ' --get-filename -o ' + d + '/%(title)s.%(ext)s '
-yt_args = ' -o ' + d + '/%(title)s.%(ext)s '
-yt_audio_args = yt_args + ' --extract-audio --audio-format mp3 -k '
 
-
-# No spaces in filename
-# yt_args = ' -o ' + d + '/%(title)s.%(ext)s ' + '--restrict-filenames '
-# yt_args = ' --get-filename -o \'' + d + '/%(title)s.%(ext)s\' ' + '--restrict-filenames '
-
-
-# This will be depreciated in the version 1.3
-def go(thread_pool):
-    """Go, go, go!
-    """
-
-    dirs = [d, f, p, c]
-
-    for _ in dirs:
-        if not os.path.exists(_):
-            os.makedirs(_)
-        if _ == d:
-            for _ in os.listdir(d):
-                pass
-                # Clean out previous downloads
-                # os.remove(d + '/' + _)
-
-    return run_queue()
-
-
-# This will be depreciated in the version 1.3
-def list_builder():
-    """Create a clean list of urls
-
-    You can put any text file or html file into files/pending
-
-    The list bulider will take any file inside of files/pending
-    and create a list of URLs from it
-    """
-
-    cookies = ops.cookie_jar(c)
-    urls = ops.read_files(p)
-
-    return urls
-
-
-# This will be depreciated in the version 1.3
-def downloader(url):
-    """Download the url
-    """
-
-    # run youtube-dl
-    # os.chmod(yt, 0o555)
-    dl = yt + yt_args + url
-    dl_audio = yt + yt_audio_args + url
-    # subprocess.Popen(url.split(), stdout=PIPE, stderr=PIPE)
-
-    t_name = threading.current_thread().getName()
-
-    try:
-        # Download file
-        # TODO: bug - Unable to parse through subprocess stdout output
-        print('[*] [' + t_name + ']', 'Running command:', dl)
-        print('[*] [' + t_name + ']', 'Downloading:', url)
-        _stdout, _stderr = subprocess.Popen(dl.split(), stdout=PIPE, stderr=PIPE).communicate()
-        print('[*] [' + t_name + ']', _stdout.decode())
-        print('[*] [' + t_name + ']', _stderr.decode())
-        # [download] Destination:
-        # regex = '^(?<=\[download\] Destination: ).*$'
-
-        # Download audio
-        # TODO: Download audio format
-        # Requires ffmpeg or avconv and ffprobe or avprobe
-        print('[*] [' + t_name + ']', 'Running command:', dl_audio)
-        print('[*] [' + t_name + ']', 'Converting to mp3:', url)
-        _stdout, _stderr = subprocess.Popen(dl_audio.split(), stdout=PIPE, stderr=PIPE).communicate()
-        print('[*] [' + t_name + ']', _stdout.decode())
-        print('[*] [' + t_name + ']', _stderr.decode())
-
-    except:
-        raise
-
-
-# This will be depreciated in the version 1.3
-def run_queue(thread_pool):
-    """Start queue processing
-    """
-
-    for _ in list_builder():
-        print('[*]', 'Added to queue:', _)
-        q.put(_)
-
-    print('[*]', q.qsize(), 'items in queue')
-
-    def threader(threads=q.qsize()):
-        """Create threads
-        """
-
-        ts = []
-        for _ in range(threads):
-            t = threading.Thread(target=worker)
-            # t.setDaemon(True)
-            t.start()
-            ts.append(t)
-
-        print('[*] Threads started', len(ts))
-
-        # Stop threads
-        for _ in range(threads):
-            q.put(None)
-
-        # Wait on threads to finish
-        for _ in ts:
-            _.join()
-
-        # Move completed downloads
-        for _, __, ___ in os.walk(d):
-            for _ in ___:
-                src = d + '/' + _
-                dst = f + '/' + _
-                print('[*] Finished', dst)
-                move(src, dst)
-
-        # print('[*] >>', '[' + t_name + ']', dl)
-        # if relpath:
-        #     move(relpath, f + '/' + dl_name_name)
-        # if dl_path:
-        #     move(dl_path, f + '/' + dl_path_name)
-
-    def worker():
-        """Create workers
-        """
-
-        while True:
-            url = q.get()
-
-            if url is None:
-                break
-
-            print('[*] Running queue:', q.qsize(), 'item(s) left')
-            thread_pool.submit(downloader, url)
-
-    return threader()
-
-
-# This will be depreciated in the version 1.3
 def main():
-    """Main
-
-    """
-    while True:
-        Youtube()
-        print('sleep for 3600')
-        time.sleep(3600)
-
-
-def test_main():
     """Main
 
     """
