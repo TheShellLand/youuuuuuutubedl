75c3b1cba6811fa2699280468139b3df525eb5d1
naisanzaa <naisanza@gmail.com>
2022-10-13 05:25:05 -0400

    fix Options

--- youuuuuuutubedl.py
+++ youuuuuuutubedl.py
@@ -80,10 +80,10 @@ class Options(object):
 
         # Youtube-dl configuration
         self._yt = os.path.join('bin', 'youtube-dl')
-        self._yt_name = f'--get-filename -o {folder}/%(title)s.%(ext)s'
-        self._yt_args = f'-o {folder}/%(title)s.%(ext)s'
+        self._yt_name = f'--get-filename -o "{folder}/%(title)s.%(ext)s"'
+        self._yt_args = f'-o "{folder}/%(title)s.%(ext)s"'
 
-        self.python = 'python'
+        self.python = 'python3'
 
         self.mp3 = mp3
 
@@ -92,19 +92,19 @@ class Options(object):
 
         if not mp3:
             if name:
-                self.dl = f"{self.python} {self._yt} -o {os.path.join(folder, name)}.%(ext)s {url}"
+                self.dl = f'{self.python} {self._yt} -o "{os.path.join(folder, name)}.%(ext)s" {url}'
             else:
-                self.dl = f'{self.python} {self._yt} -o {folder}/%(title)s.%(ext)s {url}'
+                self.dl = f'{self.python} {self._yt} -o "{folder}/%(title)s.%(ext)s" {url}'
 
         # Requires ffmpeg or avconv and ffprobe or avprobe
         # apt install ffmpeg avconv
         # apt install ffprobe avprobe
         if mp3:
             if name:
-                self.dl = f"{self._yt} -o {os.path.join(folder, name)}.%(ext)s " \
-                          f"--extract-audio --audio-format mp3 -k {url}"
+                self.dl = f'{self.python} {self._yt} -o "{os.path.join(folder, name)}.%(ext)s" ' \
+                          f'--extract-audio --audio-format mp3 -k {url}'
             else:
-                self.dl = f'{self._yt} {self._yt_args} --extract-audio --audio-format mp3 -k {url}'
+                self.dl = f'{self.python} {self._yt} {self._yt_args} --extract-audio --audio-format mp3 -k {url}'
 
     def __str__(self):
         return f'{self.dl}'
