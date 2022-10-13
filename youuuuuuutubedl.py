6afab839b9e0a71660cab8ffc7d625fb42e8f441
naisanzaa <naisanza@gmail.com>
2022-10-13 06:26:01 -0400

    refactor Youtube functions

--- youuuuuuutubedl.py
+++ youuuuuuutubedl.py
@@ -226,8 +226,8 @@ class File(object):
 
 class Youtube(object):
 
-    def __init__(self, thread_pool: int = None, urls_file: str = None):
-        """A multithreaded wrapper for youtube-dl
+    def __init__(self, max_thread_pool: int = None, urls_file: str = None):
+        """A multi-threaded wrapper for youtube-dl
         """
 
         # Directories
@@ -247,17 +247,16 @@ class Youtube(object):
         self.downloading = []
         self.finished = []
 
-        # Threading pool
-        self.pool = self._config_thread_pool(thread_pool=thread_pool)
-
-        # Queue
+        self.thread_pool = self._config_thread_pool(thread_pool=max_thread_pool)
         self.queue = Queue()
 
         self._queue_urls()
         self._start_downloads()
 
     def _config_thread_pool(self, thread_pool):
-        # Thread pool
+        """Configure threading pool
+        """
+
         if thread_pool:
             return ThreadPoolExecutor(max_workers=thread_pool)
         else:
@@ -268,15 +267,75 @@ class Youtube(object):
 
         return False
 
-    def _cpu_usage(self, max_cpu_percentage=80):
-        """Limit max cpu usage
+    def _cookie_builder(self, cookies):
+        """Create a clean list of cookies
         """
-        if psutil.cpu_percent() < max_cpu_percentage:
-            log_Youtube.debug(f'[{"cpu": >{logging_spaces}}] {psutil.cpu_percent()}%')
-            return True
+
+        cookies = self._cookie_jar(cookies)
+
+        return cookies
+
+    def _cookie_jar(self, cookie_path):
+        """Create a list of cookies
+        """
+
+        temp_c = []
+        cookies = []
+        # Open cookies
+        # Cookies exported from Google Chrome extension 'EditThisCookie'
+        for _ in os.listdir(cookie_path):
+            with open(cookie_path + '/' + _, 'r') as f:
+                cookie = f.read()
+                temp_c.append(cookie)
+
+        for _ in temp_c:
+            # TODO: bug - self._cookie_jar() should return ready-to-use cookie objects built by requests.cookies
+            cookie = json.loads(_)  # list of dicts
+
+            # TODO: bug - requests.cookies.RequestsCookieJar() is broken
+            # jar = requests.cookies.RequestsCookieJar()
+
+            if len(cookie) > 1:
+                for _ in cookie:
+                    # TODO: bug - build cookie object with requests
+                    pass
+
+        return cookies
+
+    def _download(self, url_object: Url, yt: Options):
+        """Download the url
+        """
+
+        start = int(time.time())
+
+        url = url_object.url
+        name = url_object.custom_name
+        folder = url_object.custom_folder
+
+        if yt.mp3:
+            file = File(url=url, filename=name, folder=folder, extension='.mp3')
         else:
-            log_Youtube.debug(f'[{"cpu": >{logging_spaces}}] {psutil.cpu_percent()}%')
-            return False
+            file = File(url=url, filename=name, folder=folder)
+
+        logs = LogStream()
+
+        # Download file
+        log_Youtube.debug(f'[{"downloading": >{logging_spaces}}] {name}')
+        self.downloads.append(file.add_logs(logs))
+        logs.store(self._run(yt.dl))
+
+        # get filename from logs
+        file.get_filename(self.dir_downloading)
+
+        self._finished(file)
+        log_Youtube.info(f'[{"download": >{logging_spaces}}] took {int(time.time() - start)} seconds to complete {url}')
+
+    def _finished(self, file: File):
+        """Handle finished download
+        """
+
+        log_Youtube.debug(f'[finished ] {file.filename} ({os.stat(os.path.join(self.dir_downloading, file.filename)).st_size} B)')
+        self._move_file(file)
 
     def _url_builder(self) -> [Url]:
         """Create a clean list of urls
@@ -287,7 +346,7 @@ class Youtube(object):
         and create a list of URLs from it
         """
 
-        return self._read_files(self.dir_p)
+        return self._read_files(self.dir_pending)
 
     def _read_files(self, directory) -> [Url]:
         """Read files from 'pending'
@@ -394,6 +453,105 @@ class Youtube(object):
                                     urls.append(url_object)
         return urls
 
+    def _run(self, command):
+        """Run a command
+        """
+        try:
+            log_Youtube.debug(f'[{"run": >{logging_spaces}}] {command}')
+            return self.run.run_command(command)
+            # return subprocess.Popen(command.split(), stdout=PIPE, stderr=PIPE).communicate()
+        except Exception as e:
+            log_Youtube.error(e)
+
+    def _start_downloads(self):
+        """Start downloads in thread pool
+        """
+        downloads = 0
+        sleep = 0
+        while True:
+            if self.queue.empty():
+                break
+
+            if self._max_cpu_usage(80):
+                url = self.queue.get()
+                downloads += 1
+
+                # download video
+                video_options = Options(folder=self.dir_downloading, url_object=url)
+                self.futures.append(self.thread_pool.submit(self._download, url, video_options))
+
+                # download mp3
+                mp3_options = Options(folder=self.dir_downloading, url_object=url, mp3=True)
+                self.futures.append(self.thread_pool.submit(self._download, url, mp3_options))
+                log_Youtube.info(f'[{"download": >{logging_spaces}}] ({downloads}/{urls}) {url}')
+                sleep = int(sleep / 2)
+            else:
+                sleep += int(sleep + 1 * 2)
+                # self._log.debug('[_downloader] Sleeping for: {} seconds'.format(sleep))
+                time.sleep(sleep)
+
+    def _max_cpu_usage(self, max_cpu_percentage=80):
+        """Limit max cpu usage
+        """
+        if psutil.cpu_percent() < max_cpu_percentage:
+            log_Youtube.debug(f'[{"cpu": >{logging_spaces}}] {psutil.cpu_percent()}%')
+            return True
+        else:
+            log_Youtube.debug(f'[{"cpu": >{logging_spaces}}] {psutil.cpu_percent()}%')
+            return False
+
+    def _move_file(self, file: File):
+        """Move file
+        """
+
+        filename = file.filename
+        folder = file.folder
+
+        source = os.path.join(self.dir_downloading, filename)
+
+        if not os.path.exists(source):
+            log_Youtube.error(f'[move ] source not found: {source}')
+            return False
+
+        if folder:
+            if not os.path.exists(os.path.join(self.dir_finished, folder)):
+                os.mkdir(os.path.join(self.dir_finished, folder))
+            destination = os.path.join(self.dir_finished, folder, filename)
+            destination_short = f'{os.path.split(os.path.split(destination)[0])[1]}/{os.path.split(destination)[1]}'
+        else:
+            destination = os.path.join(self.dir_finished, filename)
+            destination_short = f'{os.path.split(os.path.split(destination)[0])[1]}/{os.path.split(destination)[1]}'
+
+        try:
+            log_Youtube.debug(f'[moving ] {os.path.split(destination)[-1]} ({os.stat(source).st_size} B)')
+
+            # copy content, stat-info (mode too), timestamps...
+            shutil.copy2(source, destination)
+            # copy owner and group
+            st = os.stat(source)
+            os.chown(destination, st[stat.ST_UID], st[stat.ST_GID])
+            # os.remove(source)
+
+            log_Youtube.debug(
+                f'[moved ] {destination_short} ({os.stat(destination).st_size} B)')
+            return True
+        except Exception as e:
+            log_Youtube.error(f'[moving ] failed {os.path.split(source)[-1]}')
+            return False
+
+    def _prepare_folders(self):
+        _dirs = [self.dir_downloading, self.dir_finished, self.dir_pending, self.dir_cookies]
+
+        for directory in _dirs:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            if directory == self.dir_downloading:
+                for directory in os.listdir(self.dir_downloading):
+                    # Don't delete up previous downloads
+                    # Clean out previous downloads
+                    # os.remove(self.dir_d + '/' + directory)
+                    pass
+
     def _prepare_url(self, raw_url: str) -> tuple:
 
         if not isinstance(raw_url, str) and not raw_url:
@@ -429,124 +587,16 @@ class Youtube(object):
 
         return Url(url, name, folder)
 
-    def _run(self, command):
-        """Run a command
+    def _queue_urls(self):
+        """Add urls to queue
         """
-        try:
-            log_Youtube.debug(f'[{"run": >{logging_spaces}}] {command}')
-            return self.run.run_command(command)
-            # return subprocess.Popen(command.split(), stdout=PIPE, stderr=PIPE).communicate()
-        except Exception as e:
-            log_Youtube.error(e)
-
-    def _download(self, url_object: Url, yt: Options):
-        """Download the url
-        """
-
-        start = int(time.time())
-
-        url = url_object.url
-        name = url_object.custom_name
-        folder = url_object.custom_folder
-
-        if yt.mp3:
-            file = File(url=url, filename=name, folder=folder, extension='.mp3')
-        else:
-            file = File(url=url, filename=name, folder=folder)
-
-        logs = LogStream()
-
-        # Download file
-        log_Youtube.debug(f'[{"downloading": >{logging_spaces}}] {name}')
-        self.downloads.append(file.add_logs(logs))
-        logs.store(self._run(yt.dl))
-
-        # get filename from logs
-        file.get_filename(self.dir_d)
-
-        self._finished(file)
-        log_Youtube.info(f'[{"download": >{logging_spaces}}] took {int(time.time() - start)} seconds to complete {url}')
-
-    def _move_file(self, file: File):
-        """Move file
-        """
-
-        filename = file.filename
-        folder = file.folder
-
-        source = os.path.join(self.dir_d, filename)
-
-        if not os.path.exists(source):
-            log_Youtube.error(f'[move ] source not found: {source}')
-            return False
-
-        if folder:
-            if not os.path.exists(os.path.join(self.dir_f, folder)):
-                os.mkdir(os.path.join(self.dir_f, folder))
-            destination = os.path.join(self.dir_f, folder, filename)
-            destination_short = f'{os.path.split(os.path.split(destination)[0])[1]}/{os.path.split(destination)[1]}'
-        else:
-            destination = os.path.join(self.dir_f, filename)
-            destination_short = f'{os.path.split(os.path.split(destination)[0])[1]}/{os.path.split(destination)[1]}'
-
-        try:
-            log_Youtube.debug(f'[moving ] {os.path.split(destination)[-1]} ({os.stat(source).st_size} B)')
-
-            # copy content, stat-info (mode too), timestamps...
-            shutil.copy2(source, destination)
-            # copy owner and group
-            st = os.stat(source)
-            os.chown(destination, st[stat.ST_UID], st[stat.ST_GID])
-            # os.remove(source)
-
-            log_Youtube.debug(
-                f'[moved ] {destination_short} ({os.stat(destination).st_size} B)')
-            return True
-        except Exception as e:
-            log_Youtube.error(f'[moving ] failed {os.path.split(source)[-1]}')
-            return False
-
-    def _finished(self, file: File):
-        """Handle finished download
-        """
-
-        log_Youtube.debug(f'[finished ] {file.filename} ({os.stat(os.path.join(self.dir_d, file.filename)).st_size} B)')
-        self._move_file(file)
-
-    def _cookie_builder(self, cookies):
-        """Create a clean list of cookies
-        """
-
-        cookies = self._cookie_jar(cookies)
-
-        return cookies
-
-    def _cookie_jar(self, cookie_path):
-        """Create a list of cookies
-        """
-
-        temp_c = []
-        cookies = []
-        # Open cookies
-        # Cookies exported from Google Chrome extension 'EditThisCookie'
-        for _ in os.listdir(cookie_path):
-            with open(cookie_path + '/' + _, 'r') as f:
-                cookie = f.read()
-                temp_c.append(cookie)
-
-        for _ in temp_c:
-            # TODO: bug - self._cookie_jar() should return ready-to-use cookie objects built by requests.cookies
-            cookie = json.loads(_)  # list of dicts
-
-            # TODO: bug - requests.cookies.RequestsCookieJar() is broken
-            # jar = requests.cookies.RequestsCookieJar()
-
-            if len(cookie) > 1:
-                for _ in cookie:
-                    # TODO: bug - build cookie object with requests
-                    pass
-
-        return cookies
+        added = 0
+        urls = len(self.urls)
+        self.futures = list()
+        for url in self.urls:
+            added += 1
+            self.queue.put(url)
+            log_Youtube.info(f'[{"queue": >{logging_spaces}}] ({added}/{urls}) {url}')
 
 
 def main():
